e8dd766e26c19a9d960e7a5eb9427519eb01e660
renato <renatochavez7@gmail.com>
2024-03-16 05:47:50 -0300

    smart pointers, modern c++, VMA, etc

---
+++ vma.cpp
@@ -0,0 +1,8 @@
+//
+//  vma.cpp
+//  3DLoaderVk
+//
+//  Created by xbz on 16/03/2024.
+//
+
+#include "vma.hpp"
---
+++ vma.hpp
@@ -0,0 +1,12 @@
+//
+//  vma.hpp
+//  3DLoaderVk
+//
+//  Created by xbz on 16/03/2024.
+//
+
+#ifndef vma_h
+#define vma_h
+
+
+#endif /* vma_h */
